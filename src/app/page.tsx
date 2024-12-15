795786e8ff4b7933f2706a45ceb17eeecbe9abb6
TNDKien <kienn770@gmail.com>
2024-12-15 01:27:08 +0100

    added hero component
    added hero section with basic color styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Hero from "../components/Hero";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Hero />
+    </div>
+  );
 }
