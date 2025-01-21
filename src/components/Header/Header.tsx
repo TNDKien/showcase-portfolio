7cb4b0c8a47ff16fd386413173bc2de9ba38d055
TNDKien <kienn770@gmail.com>
2025-01-21 23:48:21 +0100

    update Header.tsx
    hover

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -22,35 +22,45 @@ export default function Header() {
   }, [prevScrollPos]);
 
   return (
-    <header
-      className={`${styles.header} ${isVisible ? styles.show : styles.hide}`}
-    >
-      <Link href="/" className={styles.profile}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/medias/KienDang.webp"
-            alt="Profile"
-            width={96}
-            height={96}
-            quality={100}
-          />
-          <span className={styles.status}></span>
-        </div>
-        <div className={styles.name}>Kien Dang</div>
-      </Link>
-      <nav className={styles.navLinks}>
-        <Link href="/#about">About me</Link>
-        <Link href="/#projects">Projects</Link>
-        <Link href="/#contact">Contact</Link>
-        <Link href="/#blog">
-          Blog
-          <FiExternalLink />
+    <>
+      <svg width="0" height="0">
+        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" stopColor="#800E13" />
+          <stop offset="10%" stopColor="#800E13" />
+          <stop offset="70%" stopColor="#7678ED" />
+          <stop offset="100%" stopColor="#7678ED" />
+        </linearGradient>
+      </svg>
+      <header
+        className={`${styles.header} ${isVisible ? styles.show : styles.hide}`}
+      >
+        <Link href="/" className={styles.profile}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/medias/KienDang.webp"
+              alt="Profile"
+              width={96}
+              height={96}
+              quality={100}
+            />
+            <span className={styles.status}></span>
+          </div>
+          <div className={styles.name}>Kien Dang</div>
         </Link>
-        {/* <a href="https://blog.kiendang.nl" className={styles.external}>
-          Blog
-          <FiExternalLink />
-        </a> */}
-      </nav>
-    </header>
+        <nav className={styles.navLinks}>
+          <Link href="/#about">About me</Link>
+          <Link href="/#projects">Projects</Link>
+          <Link href="/#contact">Contact</Link>
+          <Link href="https://blog.kiendang.nl">
+            Blog
+            <FiExternalLink />
+          </Link>
+          {/* <a href="https://blog.kiendang.nl" className={styles.external}>
+            Blog
+            <FiExternalLink />
+          </a> */}
+        </nav>
+      </header>
+    </>
   );
 }
